b292cb3210a6c17b34c1fb57cccf111c9610f9e3
cags295 <calgusa@gmail.com>
2026-04-19 11:46:24 -0600

    compile protos

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     tonic_build::configure()
         .file_descriptor_set_path(out_dir.join("limitorderbook_descriptor.bin"))
-        .compile(&["proto/limit_order_book.proto"], &["proto"])
+        .compile_protos(&["proto/limit_order_book.proto"], &["proto"])
         .unwrap();
 }
 
